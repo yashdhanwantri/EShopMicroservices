d07b18b9a29dc675581c486fd8997772830d84a7
Yash Dhanwantri <yash.dhanwantri@nagarro.com>
2024-06-12 08:42:06 +0530

    Fixed solution

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.Api.Models;
-using MediatR;
-
-namespace Catalog.Api.Products.CreateProduct
+﻿namespace Catalog.Api.Products.CreateProduct
 {
     public record CreateProductCommand(
         string Name,
@@ -13,7 +9,7 @@ namespace Catalog.Api.Products.CreateProduct
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -27,7 +23,10 @@ namespace Catalog.Api.Products.CreateProduct
                 ImageFile = command.ImageFile
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
 
 
         }
