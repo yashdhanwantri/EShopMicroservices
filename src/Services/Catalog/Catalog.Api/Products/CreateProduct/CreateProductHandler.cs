0b18a98ea0a06e35e2be02748efa6fdc70a80136
Yash Dhanwantri <yash.dhanwantri@nagarro.com>
2024-06-12 07:43:28 +0530

    Implementation for CreateProductEndpoint using CQRS

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                Price = command.Price,
+                ImageFile = command.ImageFile
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+
+
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+app.MapCarter();
+app.Run();
